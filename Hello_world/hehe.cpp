7bb38c8a54a1480584f31783e66e125c7d134399
emina <eminazolota3@gmail.com>
2018-10-25 15:32:26 +0200

    Hello world!

---
+++ Hello_world/hehe.cpp
@@ -0,0 +1,13 @@
+/*
+Language: C++
+Author: Emy
+Github: https://github.com/eminazolota
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout << "Hello World! :)" << "\n";
+	return 0;
+}
